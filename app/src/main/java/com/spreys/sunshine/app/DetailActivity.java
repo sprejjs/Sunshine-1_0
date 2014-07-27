b34675b4d3ebf7a70d35178dc215dbbc1631cd78
Vlad Spreys <vlad@spreys.com>
2014-07-27 19:56:09 +1200

    3rd lesson completed

--- app/src/main/java/com/spreys/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/spreys/sunshine/app/DetailActivity.java
@@ -1,23 +1,12 @@
 package com.spreys.sunshine.app;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.TextView;
-
-import com.spreys.sunshine.app.R;
 
 public class DetailActivity extends ActionBarActivity {
-    private static String forecastData;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +14,7 @@ public class DetailActivity extends ActionBarActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
@@ -35,6 +24,7 @@ public class DetailActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.detail, menu);
+
         return true;
     }
 
@@ -50,25 +40,5 @@ public class DetailActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-
-            Intent detailActivityIntent = getActivity().getIntent();
-            String forecastData = detailActivityIntent.getStringExtra(Intent.EXTRA_TEXT);
-            ((TextView)rootView.findViewById(R.id.detail_text)).setText(forecastData);
-            return rootView;
-        }
     }
 }
